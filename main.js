df349a9e806cc7f6c3176b192dd551efc429dd91
TechWithShreban <shreban17@gmail.com>
2023-03-19 14:27:04 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.multitext', {
+    strings: ['Hey You💗!', 'Yes You!'],
+    loop: true,
+    typeSpeed: 100,
+    backspeed: 80,
+    backDelay:1500,
+  });
---
+++ main3.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.multitext', {
+    strings: ['Hope this made you smile😚'],
+    typeSpeed: 150,
+    backspeed: 80,
+    // backDelay:1500,
+  });
