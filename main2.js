f45b9e040fbd632f91e90dea7da1416bfad9d967
TechWithShreban <shreban17@gmail.com>
2023-03-21 21:16:13 +0530

    modified:   end.html modified:   index.html modified:   main.html modified:   main.js modified:   main2.js modified:   main3.js modified:   paragraph.html modified:   style.css

--- main2.js
+++ main2.js
@@ -1,6 +1,6 @@
 var typed = new Typed('.multitext', {
     strings: ['Now that you are here, i want to say, you are the most beautiful person I have seen😊'],
-    typeSpeed: 150,
+    typeSpeed: 80,
     backspeed: 80,
     // backDelay:1500,
   });
--- main3.js
+++ main3.js
@@ -1,6 +1,6 @@
 var typed = new Typed('.multitext', {
     strings: ['Hope this made you smile😚'],
-    typeSpeed: 150,
+    typeSpeed: 100,
     backspeed: 80,
     // backDelay:1500,
   });
